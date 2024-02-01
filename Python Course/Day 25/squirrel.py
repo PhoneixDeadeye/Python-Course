9590fcc93ece43d41f500de12abe6a6ea8aa4c1d
Rohan Agarwal <115068883+PhoneixDeadeye@users.noreply.github.com>
2024-02-02 00:55:04 +0530

    Add files via upload

---
+++ Python Course/Day 25/squirrel.py
@@ -0,0 +1,18 @@
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+cinnamon_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+print(gray_squirrels_count)
+print(cinnamon_squirrels_count)
+print(black_squirrels_count)
+
+
+data_dict = {
+    "Fur olor": ["Gray", "Cinnamonn", "Black"],
+    "Count": [gray_squirrels_count, cinnamon_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrels_count.csv")
